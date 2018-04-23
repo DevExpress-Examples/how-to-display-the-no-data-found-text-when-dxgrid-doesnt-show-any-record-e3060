47ebb24d8acb70c26c7e50cbe013b089b81d34d7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SLGridExample/VisibleRowsCountHelper.cs
@@ -0,0 +1,40 @@
+﻿// Developer Express Code Central Example:
+// How to display a line when the DXGrid doesn't include any record
+// 
+// This sample shows how to display a line within the grid's view if it does not
+// contain any rows.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3060
+
+using System;
+using System.Windows;
+using DevExpress.Data.Browsing;
+using DevExpress.Xpf.Grid;
+
+namespace SLGridExample {
+    public class VisibleRowsCountHelper {
+
+        public static readonly DependencyProperty HasVisibleRowsProperty =
+            DependencyProperty.RegisterAttached("HasVisibleRows", typeof(bool), typeof(VisibleRowsCountHelper), new PropertyMetadata(false));
+
+        public static bool GetHasVisibleRows(DependencyObject obj) {
+            return (bool)obj.GetValue(HasVisibleRowsProperty);
+        }
+
+        public static void SetHasVisibleRows(DependencyObject obj, bool value) {
+            obj.SetValue(HasVisibleRowsProperty, value);
+        }
+
+        public static readonly DependencyProperty EmptyGridMessageProperty =
+            DependencyProperty.RegisterAttached("EmptyGridMessage", typeof(string), typeof(VisibleRowsCountHelper), new PropertyMetadata(""));
+
+        public static string GetEmptyGridMessage(DependencyObject obj) {
+            return (string)obj.GetValue(EmptyGridMessageProperty);
+        }
+
+        public static void SetEmptyGridMessage(DependencyObject obj, string value) {
+            obj.SetValue(EmptyGridMessageProperty, value);
+        }
+    }
+}
---
+++ VB/SLGridExample/MainPage.xaml.vb
@@ -0,0 +1,43 @@
+﻿' Developer Express Code Central Example:
+' How to display a line when the DXGrid doesn't include any record
+' 
+' This sample shows how to display a line within the grid's view if it does not
+' contain any rows.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E3060
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows.Controls
+Imports System.Windows
+
+Namespace SLGridExample
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub grid_PropertyChanged(ByVal sender As Object, ByVal e As System.ComponentModel.PropertyChangedEventArgs)
+			If e.PropertyName = "VisibleRowCount" Then
+				grid.View.SetValue(VisibleRowsCountHelper.HasVisibleRowsProperty, grid.VisibleRowCount <> 0)
+			End If
+		End Sub
+
+		Private Sub grid_ItemsSourceChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.ItemsSourceChangedEventArgs)
+			grid.View.SetValue(VisibleRowsCountHelper.HasVisibleRowsProperty, e.NewItemsSource IsNot Nothing)
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            CType(grid.DataContext, PersonsViewModel).RemovePerson(grid.SelectedItem)
+		End Sub
+
+		Private Sub Button_Click_1(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			grid.ItemsSource = Nothing
+		End Sub
+
+
+	End Class
+End Namespace
