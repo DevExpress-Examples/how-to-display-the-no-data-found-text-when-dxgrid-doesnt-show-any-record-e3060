697658935d730637d555809c54c39b7802ac3baf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/EmptyGridTemplate/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace EmptyGridTemplate {
+    public partial class MainPage : UserControl {
+
+        public static readonly DependencyProperty IsGridEmptyProperty = DependencyProperty.RegisterAttached("IsGridEmpty", typeof(bool), typeof(MainPage), new PropertyMetadata(false));
+
+        public static bool GetIsGridEmpty(DependencyObject dependencyObject) {
+            return (bool)dependencyObject.GetValue(IsGridEmptyProperty);
+        }
+        public static void SetIsGridEmpty(DependencyObject dependencyObject, bool value) {
+            dependencyObject.SetValue(IsGridEmptyProperty, value);
+        }
+
+        ObservableCollection<TestData> data;
+
+        public MainPage() {
+            InitializeComponent();
+
+            data = new ObservableCollection<TestData>();
+            for (int i = 0; i < 5; i++)
+                data.Add(new TestData() {
+                    Text = "Row" + i, Number = i, Group = i % 5
+                });
+
+           grid.DataSource = data;
+        }
+
+        void grid_LayoutUpdated(object sender, EventArgs e) {
+            MainPage.SetIsGridEmpty(grid, grid.VisibleRowCount == 0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            data.Clear();
+        }
+    }
+
+    public class TestData {
+        public string Text {
+            get;
+            set;
+        }
+        public int Number {
+            get;
+            set;
+        }
+        public int Group {
+            get;
+            set;
+        }
+    }
+}
---
+++ VB/EmptyGridTemplate/MainPage.xaml.vb
@@ -0,0 +1,79 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports System.Collections.ObjectModel
+
+Namespace EmptyGridTemplate
+	Partial Public Class MainPage
+		Inherits UserControl
+
+		Public Shared ReadOnly IsGridEmptyProperty As DependencyProperty = DependencyProperty.RegisterAttached("IsGridEmpty", GetType(Boolean), GetType(MainPage), New PropertyMetadata(False))
+
+		Public Shared Function GetIsGridEmpty(ByVal dependencyObject As DependencyObject) As Boolean
+			Return CBool(dependencyObject.GetValue(IsGridEmptyProperty))
+		End Function
+		Public Shared Sub SetIsGridEmpty(ByVal dependencyObject As DependencyObject, ByVal value As Boolean)
+			dependencyObject.SetValue(IsGridEmptyProperty, value)
+		End Sub
+
+		Private data As ObservableCollection(Of TestData)
+
+		Public Sub New()
+			InitializeComponent()
+
+			data = New ObservableCollection(Of TestData)()
+			For i As Integer = 0 To 4
+				data.Add(New TestData() With {.Text = "Row" & i, .Number = i, .Group = i Mod 5})
+			Next i
+
+		   grid.DataSource = data
+		End Sub
+
+		Private Sub grid_LayoutUpdated(ByVal sender As Object, ByVal e As EventArgs)
+			MainPage.SetIsGridEmpty(grid, grid.VisibleRowCount = 0)
+		End Sub
+
+		Private Sub Button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			data.Clear()
+		End Sub
+	End Class
+
+	Public Class TestData
+		Private privateText As String
+		Public Property Text() As String
+			Get
+				Return privateText
+			End Get
+			Set(ByVal value As String)
+				privateText = value
+			End Set
+		End Property
+		Private privateNumber As Integer
+		Public Property Number() As Integer
+			Get
+				Return privateNumber
+			End Get
+			Set(ByVal value As Integer)
+				privateNumber = value
+			End Set
+		End Property
+		Private privateGroup As Integer
+		Public Property Group() As Integer
+			Get
+				Return privateGroup
+			End Get
+			Set(ByVal value As Integer)
+				privateGroup = value
+			End Set
+		End Property
+	End Class
+End Namespace
